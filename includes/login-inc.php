ad1773b55cf597d9ff71385e60e5c58c218ad5a4
Mohammed-Ali45 <mohammed70011@hotmail.com>
2023-11-21 16:24:32 +1100

    added connection for Moey's db, commented out Vics

--- includes/login-inc.php
+++ includes/login-inc.php
@@ -1,40 +1,45 @@
 <?php
 session_start();
 
-if(isset($_POST["submit"])) {
+if (isset($_POST["submit"])) {
 
     $email = $_POST["email"];
     $password = $_POST["password"];
 
-    //will the be the connection that will open up the database
-    $conn = odbc_connect('z5259813', '', '', SQL_CUR_USE_ODBC); 
+    /*will the be the connection that will open up the database 
+    1 of the 3 code blocks below should be uncommented according to who is currently running */
+
+    //Victoria
+    //$conn = odbc_connect('z5259813', '', '', SQL_CUR_USE_ODBC);
+
+    //Moey
+    $conn = odbc_connect("Driver= {Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\dev\Mutation.accdb", '', '', SQL_CUR_USE_ODBC);
 
     //initialize uid = false
     $uidExists = false;
 
     $sql = "SELECT * FROM users WHERE usersEmail = '$email' AND usersPwd = '$password' ";
 
-    $exists = odbc_exec($conn,$sql);
+    $exists = odbc_exec($conn, $sql);
 
-    if(odbc_fetch_row($exists)){ //if the email and password exist in the same row then the user 
+    if (odbc_fetch_row($exists)) { //if the email and password exist in the same row then the user 
         $uidExists = true; //change uid = true
     }
 
     if (empty($email) || empty($password) || ($uidExists == false)) { //checking for empty inputs and matching users information
         header('location: http://engpwws005/z5259813$/BIOM9450_Major/login-p.php?error=help');
         exit;
-    }else{ //if correct will redirect to the home page
-        $exists = odbc_exec($conn,$sql);
+    } else { //if correct will redirect to the home page
+        $exists = odbc_exec($conn, $sql);
         $row = odbc_fetch_array($exists);
-        
+
         //storing email of the user in the session variable
         $_SESSION['username'] = $row['usersName'];
         $_SESSION['email'] = $row['usersEmail'];
         header('location: http://engpwws005/z5259813$/BIOM9450_Major/patient.php');
         exit;
     }
-}
-else {
+} else {
     header("location: http://engpwws005/z5259813$/BIOM9450_Major/login-p.php?error=help");
     exit();
 }
