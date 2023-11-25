7c6f458f98e69c25ab56ad1c09966d030912ebcc
Mohammed-Ali45 <mohammed70011@hotmail.com>
2023-11-25 20:49:39 +1100

    fixed queries for rep genes & muts in test files

---
+++ test_rep_gene.php
@@ -0,0 +1,163 @@
+<?php
+
+session_start();
+
+// Victoria's db connection
+//$conn = odbc_connect('z5259813', '', '', SQL_CUR_USE_ODBC); 
+//$conn = odbc_connect("Driver= {Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\User\Downloads\UNSW\Current\BIOM9450\Mutation.accdb", "", "", SQL_CUR_USE_ODBC);
+
+//Moey's db connection
+$conn = odbc_connect("Driver= {Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\dev\Mutation.accdb", '', '', SQL_CUR_USE_ODBC);
+
+
+include_once 'header.php'
+    ?>
+
+<!-- Some placeholder html to know everything is working-->
+
+<body>
+    <!--setting up the nav bar-->
+    <header>
+        <div class="container1">
+            <img src="images/snail.jpg" alt="logo" class="logo" width="70" height="70">
+            <nav>
+                <ul>
+                    <li><a href="researcher.php">Database</a></li>
+                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="includes/logout-inc.php" class="dropbtn">Logout</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <!--the body section-->
+    <?php
+
+
+
+
+
+    // Here's the first query
+    $queryone = "SELECT DISTINCT
+    Patient.PatientID,
+    Patient.FirstName,
+    Patient.LastName,
+    Mutation.gene_affected
+INTO
+    TT_pat_affgenes
+FROM
+    (
+        Specimens
+        INNER JOIN Patient ON Specimens.[icgc_specimen_id] = Patient.[icgc_specimen_id]
+    )
+    INNER JOIN (
+        Mutation
+        INNER JOIN SpecimenMutations ON Mutation.[mutationID] = SpecimenMutations.[mutationID]
+    ) ON Specimens.[icgc_specimen_id] = SpecimenMutations.[icgc_specimen_id]
+WHERE
+    ISNULL(gene_affected) = FALSE
+ORDER BY
+    Mutation.gene_affected";
+
+    //Just in case, execute them one at a time
+    $patients_affected_genes = odbc_exec($conn, $queryone);
+
+
+    // Here's the second query
+    $querytwo = "SELECT
+    Mutation.gene_affected
+INTO
+    TT_rep_genes
+FROM
+    pat_genes1
+GROUP BY
+    Mutation.gene_affected
+HAVING
+    COUNT(gene_affected) > 1";
+
+
+
+    //gonna try executing the two queries first, then referencing those in the join statement
+    
+    $repeating_genes = odbc_exec($conn, $querytwo);
+
+
+
+
+
+    // The big one, the 3rd query
+    $querythree = "SELECT
+    TT_pat_affgenes.PatientID,
+    TT_pat_affgenes.FirstName,
+    TT_pat_affgenes.LastName,
+    TT_rep_genes.gene_affected
+FROM
+    TT_pat_affgenes
+    INNER JOIN TT_rep_genes ON TT_pat_affgenes.gene_affected = TT_rep_genes.gene_affected";
+
+
+
+
+
+    //Execute the last query
+    $only_repeating_genes = odbc_exec($conn, $querythree);
+
+
+    //Let's count the rows
+    $gene_count_query = "SELECT COUNT(*) AS gene_count FROM ($querythree)";
+    $gene_count = odbc_result(odbc_exec($conn, $gene_count_query), 'gene_count');
+
+
+    //Okay let's try printing out the results of this array and see if they stick around
+    ?>
+
+    <table class="tablestyle" id="tbl1">
+        <thead>
+            <tr>
+                <th style="width:15%">Mutation ID</th>
+                <th>Gene Involved</th>
+                <th>Location</th>
+                <th>Potential Impact</th>
+            </tr>
+        </thead>
+        <tbody id="tbody1">
+            <?php
+            for ($gene_no = 1; $gene_no <= $gene_count; $gene_no++) {
+                $gene_row = odbc_fetch_array($only_repeating_genes, $gene_no);
+                echo '<tr>';
+                echo '<td>' . $gene_row['gene_affected'] . '</td>';
+                echo ' ';
+                echo '<td>' . $gene_row['PatientID'] . '</td>';
+                echo '<td>' . $gene_row['FirstName'] . '</td>';
+                echo '<td>' . $gene_row['LastName'] . '</td>';
+                echo '</tr>';
+            }
+
+            ?>
+        </tbody>
+    </table>
+
+
+
+
+
+
+
+
+    <?php
+
+    // This releases the lock on the database for some reason, no querying beyond this point.
+    $free_result3 = odbc_free_result($only_repeating_genes);
+
+    //Deletes the temporary tables before moving on
+    $delete_table1 = odbc_exec($conn, "DROP TABLE TT_pat_affgenes;");
+    $delete_table2 = odbc_exec($conn, "DROP TABLE TT_rep_genes;");
+
+    ?>
+
+    <?php
+    include_once 'footer.php'
+        ?>
+    <?php
+
+    ?>
---
+++ test_rep_mut.php
@@ -0,0 +1,162 @@
+<?php
+
+session_start();
+
+// Victoria's db connection
+//$conn = odbc_connect('z5259813', '', '', SQL_CUR_USE_ODBC); 
+//$conn = odbc_connect("Driver= {Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\User\Downloads\UNSW\Current\BIOM9450\Mutation.accdb", "", "", SQL_CUR_USE_ODBC);
+
+//Moey's db connection
+$conn = odbc_connect("Driver= {Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\dev\Mutation.accdb", '', '', SQL_CUR_USE_ODBC);
+
+
+include_once 'header.php'
+    ?>
+
+<!-- Some placeholder html to know everything is working-->
+
+<body>
+    <!--setting up the nav bar-->
+    <header>
+        <div class="container1">
+            <img src="images/snail.jpg" alt="logo" class="logo" width="70" height="70">
+            <nav>
+                <ul>
+                    <li><a href="researcher.php">Database</a></li>
+                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="includes/logout-inc.php" class="dropbtn">Logout</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <!--the body section-->
+    <?php
+
+
+
+
+
+    // Here's the first query
+    $queryone = "SELECT
+    Patient.PatientID,
+    Patient.FirstName,
+    Patient.LastName,
+
+    SpecimenMutations.mutationID
+INTO
+    Table1
+FROM
+    (
+        Specimens
+        INNER JOIN SpecimenMutations ON Specimens.[icgc_specimen_id] = SpecimenMutations.[icgc_specimen_id]
+    )
+    INNER JOIN Patient ON Specimens.[icgc_specimen_id] = Patient.[icgc_specimen_id]";
+
+
+    // Here's the second query
+    $querytwo = "SELECT
+    SpecimenMutations.mutationID
+INTO
+    Table2
+FROM
+    (
+        Specimens
+        INNER JOIN SpecimenMutations ON Specimens.[icgc_specimen_id] = SpecimenMutations.[icgc_specimen_id]
+    )
+    INNER JOIN Patient ON Specimens.[icgc_specimen_id] = Patient.[icgc_specimen_id]
+GROUP BY
+    SpecimenMutations.mutationID
+HAVING
+    COUNT(SpecimenMutations.mutationID) > 1";
+
+
+
+    //gonna try executing the two queries first, then referencing those in the join statement
+    $patients_mutations = odbc_exec($conn, $queryone);
+    $repeating_mutations = odbc_exec($conn, $querytwo);
+
+
+
+
+
+    // The big one, the 3rd query
+    $querythree = "SELECT
+    Table2.mutationID,
+    Table1.PatientID,
+    Table1.FirstName,
+    Table1.LastName
+FROM
+    (
+        Mutation
+        INNER JOIN Table1 ON Mutation.[mutationID] = Table1.[mutationID]
+    )
+    INNER JOIN Table2 ON Mutation.[mutationID] = Table2.[mutationID]
+    ORDER BY Table2.mutationID";
+
+
+
+
+
+    //Execute the last query
+    $only_repeating_mutations = odbc_exec($conn, $querythree);
+
+
+    //Let's count the rows
+    $mutation_count_query = "SELECT COUNT(*) AS mutation_count FROM ($querythree)";
+    $mutation_count = odbc_result(odbc_exec($conn, $mutation_count_query), 'mutation_count');
+
+
+    //Okay let's try printing out the results of this array and see if they stick around
+    ?>
+
+    <table class="tablestyle" id="tbl1">
+        <thead>
+            <tr>
+                <th style="width:15%">Mutation ID</th>
+                <th>Gene Involved</th>
+                <th>Location</th>
+                <th>Potential Impact</th>
+            </tr>
+        </thead>
+        <tbody id="tbody1">
+            <?php
+            for ($mutation_no = 1; $mutation_no <= $mutation_count; $mutation_no++) {
+                $mutation_row = odbc_fetch_array($only_repeating_mutations, $mutation_no);
+                echo '<tr>';
+                echo '<td>' . $mutation_row['mutationID'] . '</td>';
+                echo ' ';
+                echo '<td>' . $mutation_row['PatientID'] . '</td>';
+                echo '<td>' . $mutation_row['FirstName'] . '</td>';
+                echo '<td>' . $mutation_row['LastName'] . '</td>';
+                echo '</tr>';
+            }
+
+            ?>
+        </tbody>
+    </table>
+
+
+
+
+
+
+
+
+    <?php
+
+    // This releases the lock on the database for some reason, no querying beyond this point.
+    $free_result3 = odbc_free_result($only_repeating_mutations);
+
+    //Deletes the temporary tables before moving on
+    $delete_table1 = odbc_exec($conn, "DROP TABLE Table1;");
+    $delete_table2 = odbc_exec($conn, "DROP TABLE Table2;");
+
+    ?>
+
+    <?php
+    include_once 'footer.php'
+        ?>
+    <?php
+
+    ?>
