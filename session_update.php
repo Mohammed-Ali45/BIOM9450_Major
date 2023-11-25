e36fb10398fd958dd1607af4dd54711629acae92
Mohammed-Ali45 <mohammed70011@hotmail.com>
2023-11-25 22:01:58 +1100

    fixed researcher redirects to patient & profile

--- session_update.php
+++ session_update.php
@@ -6,7 +6,7 @@ $conn = odbc_connect("Driver= {Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\
 
 
 //Gets PatientID from URL
-$patient_id = $_GET['patientid'][0];
+$patient_id = $_GET['patientid'];
 
 $patient_data_query = "SELECT * FROM Patient WHERE PatientID = $patient_id";
 $patient_data = odbc_exec($conn, $patient_data_query);
@@ -15,5 +15,13 @@ $patient_email = odbc_result($patient_data, 'Email');
 
 
 $_SESSION['Patient_email'] = $patient_email;
-header('location: /patient.php');
-exit;
+
+
+// The switchboard
+if ($_GET['destination'] == 'profile') {
+    header('location: /profile.php');
+    exit;
+} else if ($_GET['destination'] == 'patient') {
+    header('location: /patient.php');
+    exit;
+}
