8e2356d8c16cbd75f4b6a6deeed551dd1503ffb3
toriatoast <victoria.inthavong@gmail.com>
2023-11-26 22:24:42 +1100

    about login button added

--- about.php
+++ about.php
@@ -19,15 +19,7 @@ include_once 'header.php'
           <li><a href="index.php">Home</a></li>
           <li><a href="about.php">About</a></li>
           <li><a href="database.php">Database</a></li>
-          <li>
-            <div class="dropdown">
-              <a class="dropbtn">Login</a>
-              <div class="dropdown-content">
-                <a href="login-p.php">Patient</a>
-                <a href="login-r.php">Staff</a>
-              </div>
-            </div>
-          </li>
+          <li><a class="dropbtn" href ="loging.php">Login</a></li>
         </ul>
       </nav>
     </div>
--- test.php
+++ test.php
@@ -6,148 +6,125 @@ if (isset($_SESSION['email'])) {
     $email = $_SESSION['email'];
     // Victoria's db connection
     //$conn = odbc_connect('z5259813', '', '', SQL_CUR_USE_ODBC); 
-    //$conn = odbc_connect("Driver= {Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\User\Downloads\UNSW\Current\BIOM9450\Mutation.accdb", "", "", SQL_CUR_USE_ODBC);
-    if(!extension_loaded('pdo')) {
-        dl('php_pdo.dll');
-        dl('php_pdo_odbc.dll');
-      }
-      
-      $dsn = 'Driver= {Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\User\Downloads\UNSW\Current\BIOM9450\Mutation.accdb';
-      
-      $conn = new PDO($dsn, '', '');
+    $conn = odbc_connect("Driver= {Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\User\Downloads\UNSW\Current\BIOM9450\Mutation.accdb", "", "", SQL_CUR_USE_ODBC);
+    
     //Moey's db connection
     //$conn = odbc_connect("Driver= {Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\dev\Mutation.accdb", '', '', SQL_CUR_USE_ODBC);
 
 
+    
+            //This code block retrieves the data for viewing affected genes that appear in more than one patient
+            // 1st query retrieves all affected genes in all patients
+            $all_affected_genes = "SELECT DISTINCT
+                        Patient.PatientID,
+                        Patient.FirstName,
+                        Patient.LastName,
+                        Mutation.gene_affected
+                        INTO
+                        TT_pat_affgenes
+                        FROM
+                        (
+                            Specimens
+                            INNER JOIN Patient ON Specimens.[icgc_specimen_id] = Patient.[icgc_specimen_id]
+                        )
+                        INNER JOIN (
+                            Mutation
+                            INNER JOIN SpecimenMutations ON Mutation.[mutationID] = SpecimenMutations.[mutationID]
+                        ) ON Specimens.[icgc_specimen_id] = SpecimenMutations.[icgc_specimen_id]
+                        WHERE
+                        ISNULL(gene_affected) = FALSE
+                        ORDER BY
+                        Mutation.gene_affected";
+
+            //1st query executed
+            $patients_affected_genes = odbc_exec($conn, $all_affected_genes);
+
+
+            // 2nd query lists all genes that repeat
+            $repeating_genes_index = "SELECT
+                        Mutation.gene_affected
+                        INTO
+                        TT_rep_genes
+                        FROM
+                        TT_pat_affgenes
+                        GROUP BY
+                        Mutation.gene_affected
+                        HAVING
+                        COUNT(gene_affected) > 1";
+
+            // Executes 2nd query
+            $repeating_genes = odbc_exec($conn, $repeating_genes_index);
 
 
 
-    //grabbing patient table
-    $patient_data_query = "SELECT Patient.[PatientID], Patient.[FirstName], Patient.[LastName], Patient.[icgc_specimen_id] FROM Patient";
-    $patient_data_table = odbc_exec($conn, $patient_data_query);
-    $patient_data = odbc_fetch_array($patient_data_table);
 
-    $patient_count_query = "SELECT COUNT(*) AS patient_count FROM ($patient_data_query);";
-    $patient_count = odbc_result(odbc_exec($conn, $patient_count_query), 'patient_count');
 
-    include_once 'header.php'
+            /* 3rd query produces the desired table only with repeating genes and
+            respective patient data */
+            $repeating_affected_genes = "SELECT
+                                TT_pat_affgenes.PatientID,
+                                TT_pat_affgenes.FirstName,
+                                TT_pat_affgenes.LastName,
+                                TT_rep_genes.gene_affected
+                            FROM
+                                TT_pat_affgenes
+                            INNER JOIN TT_rep_genes ON TT_pat_affgenes.gene_affected = TT_rep_genes.gene_affected";
 
-        ?>
 
-    <body>
-        <!--setting up the nav bar-->
-        <header>
-            <div class="container1">
-                <img src="images/snail.jpg" alt="logo" class="logo" width="70" height="70">
-                <nav>
-                    <ul>
-                        <li><a href="researcher.php">Database</a></li>
-                        <li><a href="profile.php">Profile</a></li>
-                        <li><a href="includes/logout-inc.php" class="dropbtn">Logout</a></li>
-                    </ul>
-                </nav>
+            // Executes 3rd query
+            $only_repeating_genes = odbc_exec($conn, $repeating_affected_genes);
+
+
+            // Counts rows in the resulting table
+            $gene_count_query = "SELECT COUNT(*) AS gene_count FROM ($repeating_affected_genes)";
+            $gene_count = odbc_result(odbc_exec($conn, $gene_count_query), 'gene_count');
+            ?>
+
+            <div class = "card-single">
+                <div class = "card">
+                    <h1>Affected Genes</h1>
+                    <!--Prints the table produced in the above 3 queries for repeating affected genes-->
+                    <table class="tablestyle" id="tbl1">
+                        <thead>
+                            <tr>
+                                <th style="width:15%">Mutation ID</th>
+                                <th>Gene Involved</th>
+                                <th>Location</th>
+                                <th>Potential Impact</th>
+                            </tr>
+                        </thead>
+                        <tbody id="tbody1">
+                            <?php
+                            for ($gene_no = 1; $gene_no <= $gene_count; $gene_no++) {
+                                $gene_row = odbc_fetch_array($only_repeating_genes, $gene_no);
+                                echo '<tr>';
+                                echo '<td>' . $gene_row['gene_affected'] . '</td>';
+                                echo ' ';
+                                echo '<td>' . $gene_row['PatientID'] . '</td>';
+                                echo '<td>' . $gene_row['FirstName'] . '</td>';
+                                echo '<td>' . $gene_row['LastName'] . '</td>';
+                                echo '</tr>';
+                            }
+
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </header>
 
-        <!--the body section-->
-        <?php 
-        //grabbing patient table
-        // $patient_data_query = "SELECT Patient.[PatientID], Patient.[FirstName], Patient.[LastName], Patient.[icgc_specimen_id] FROM Patient";
-        // $patient_data_table = odbc_exec($conn, $patient_data_query);
-        // $patient_data = odbc_fetch_array($patient_data_table);
+            <?php
 
-        // $patient_count_query = "SELECT COUNT(*) AS patient_count FROM ($patient_data_query);";
-        // $patient_count = odbc_result(odbc_exec($conn, $patient_count_query), 'patient_count');
+            // Releases db lock, end of repeating affected gene data retrieval
+            $free_muts = odbc_free_result($only_repeating_genes);
 
-        // $patient_mutations = "SELECT
-        //                         Mutation.mutationID,
-        //                         Patient.PatientID,
-        //                         Patient.FirstName,
-        //                         Patient.LastName,
-        //                         Mutation.chromosome,
-        //                         Mutation.chromosome_start,
-        //                         Mutation.chromosome_end,
-        //                         Mutation.mutated_from_allele,
-        //                         Mutation.mutated_to_allele
-        //                     FROM
-        //                         (
-        //                             Specimens
-        //                             INNER JOIN Patient ON Specimens.[icgc_specimen_id] = Patient.[icgc_specimen_id]
-        //                         )
-        //                         INNER JOIN (
-        //                             Mutation
-        //                             INNER JOIN SpecimenMutations ON Mutation.[mutationID] = SpecimenMutations.[mutationID]
-        //                         ) ON Specimens.[icgc_specimen_id] = SpecimenMutations.[icgc_specimen_id]
-        //                     WHERE
-        //                         Mutation.mutationID IN (
-        //                             SELECT
-        //                                 Mutation.mutationID
-        //                             FROM
-        //                                 (
-        //                                     Specimens
-        //                                     INNER JOIN Patient ON Specimens.[icgc_specimen_id] = Patient.[icgc_specimen_id]
-        //                                 )
-        //                                 INNER JOIN (
-        //                                     Mutation
-        //                                     INNER JOIN SpecimenMutations ON Mutation.[mutationID] = SpecimenMutations.[mutationID]
-        //                                 ) ON Specimens.[icgc_specimen_id] = SpecimenMutations.[icgc_specimen_id]
-        //                             GROUP BY
-        //                                 Mutation.mutationID
-        //                             HAVING
-        //                                 COUNT(Mutation.mutationID) > 1
-        //                         )";
-        // $data = odbc_exec( $conn, "EXEC Query6" );
-        // // $data = odbc_exec( $conn, "EXEC Query7" );
-        // //$table = odbc_result($data , 'Repeat');
-        // $row = odbc_fetch_array($data);                              
-        // echo print_r($row);
-        // $all_rows = array();
-        // while($row = odbc_fetch_array($data)) {
-        // $all_rows[] = $row;
-        // }
-        // echo print_r($all_rows);
-
-        $stmt = $conn->prepare("EXEC Query6");
-        $stmt->execute();
-
-        $all_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        echo print_r($all_rows);
+            /*Deletes the temporary tables before moving on, allows them to be
+            made again on subsquent loads of this page*/
+            $delete_TT_pat_affgenes = odbc_exec($conn, "DROP TABLE TT_pat_affgenes;");
+            $delete_TT_rep_genes = odbc_exec($conn, "DROP TABLE TT_rep_genes;");
+            ?>
 
 
-        // $patient_mutations_table = odbc_exec($conn, $patient_mutations);
-        // $patient_mutations_data = odbc_fetch_array($patient_mutations_table);
-        // echo odbc_num_rows($patient_mutations_table);
-        // while(odbc_fetch_row($patient_mutations_table))
-        //     {
-        //     echo odbc_result($patient_mutations_table, 1);
-        //     }
-        //$patient_mutations_count_row = "SELECT COUNT(*) AS patient_mutations_count FROM ($patient_mutations) AS test";
-
-        //$test = odbc_exec($conn, $patient_mutations_count_row);
-        //$test2 = odbc_result($test, 'patient_mutations_count');
-        
-        //$patient_mutations_count = odbc_result(odbc_exec($conn, $patient_mutations_count_row), 'patient_mutations_count');
-        
-        
-        // ?>
-        <!-- // <h1>Patient Database</h1>
-        //             <form>
-        //                  search bar 
-        //                 <input type="text" id="searchbar" placeholder="Search Here..." style="width:100%">
-        //             </form>
-        //             <button id="newpatient">Add a new patient</button>
-        //             <table class="tablestyle" id="tbl1">
-        //                 <thead>
-        //                     <tr>
-        //                         <th style="width:15%">Patient ID</th>
-        //                         <th>Mutation ID</th>
-        //                         <th>First Name</th>
-        //                         <th>Last Name</th>
-        //                     </tr>
-        //                 </thead>
-        //                 <tbody id="tbody1"> -->
- 
+   
             <?php
             include_once 'footer.php'
                 ?>
